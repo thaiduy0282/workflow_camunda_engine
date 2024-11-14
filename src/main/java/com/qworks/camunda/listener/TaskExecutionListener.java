c6a675d1ce682e5df70129e19be3949a3633ff58
dungkt1 <dungkt1@vnpay.vn>
2024-11-14 15:04:29 +0700

    [dungkt] using redis to store process variables

--- src/main/java/com/qworks/camunda/listener/TaskExecutionListener.java
+++ src/main/java/com/qworks/camunda/listener/TaskExecutionListener.java
@@ -8,17 +8,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.BooleanUtils;
 import org.camunda.bpm.engine.ProcessEngines;
 import org.camunda.bpm.engine.RuntimeService;
-import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.ExecutionListener;
-import org.camunda.bpm.engine.impl.context.Context;
 import org.camunda.bpm.engine.impl.persistence.entity.ExecutionEntity;
-import org.camunda.bpm.engine.task.Task;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.util.Date;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @Component("taskExecutionListener")
@@ -27,6 +24,8 @@ import java.util.Optional;
 public class TaskExecutionListener implements ExecutionListener {
 
     private final WorkflowApiClient workflowApiClient;
+
+    private final RedisTemplate<String, Object> redisTemplate;
 
     @Override
     public void notify(DelegateExecution execution) {
@@ -37,28 +36,28 @@ public class TaskExecutionListener implements ExecutionListener {
         String processDefinition = ((ExecutionEntity) execution).getProcessDefinition().getKey();
         log.info("Event: {} for task: {}", eventName, activityName);
         log.info("processDefinition: {}", processDefinition);
-        UpdateProcessRequest.UpdateProcessRequestBuilder updateProcessRequest =  UpdateProcessRequest.builder()
+        UpdateProcessRequest.UpdateProcessRequestBuilder updateProcessRequest = UpdateProcessRequest.builder()
                 .processInstanceId(processInstanceId)
                 .nodeId(activityId)
                 .activityName(activityName)
                 .startTime(Optional.of(new Date()))
                 .endTime(Optional.of(new Date()));
 
+        String redisKey = "process:variables:" + processInstanceId;
         switch (eventName) {
             case "start", "assign":
                 updateProcessRequest.status(ProcessStatus.RUNNING);
                 log.info("call client to create process history with process definition id {} successfully!", processDefinition);
                 break;
             case "end", "complete":
-                Map<String, Object> globalVariables = execution.getVariables();
-                Boolean isFailed = (Boolean) globalVariables.get("isFailed");
+                Boolean isFailed = (Boolean) redisTemplate.opsForHash().get(redisKey, "isFailed");
                 if (BooleanUtils.isTrue(isFailed)) {
                     RuntimeService runtimeService = ProcessEngines.getDefaultProcessEngine().getRuntimeService();
                     String processInstanceId1 = execution.getProcessInstanceId();
                     runtimeService.deleteProcessInstance(processInstanceId1, "Process terminated by ExecutionListener");
                     updateProcessRequest.status(ProcessStatus.FAILED);
-                    String errMsg = (String) globalVariables.get("errMessage");
-                    updateProcessRequest.note(errMsg.describeConstable());
+                    String errMsg = (String) redisTemplate.opsForHash().get(redisKey, "errMessage");
+                    updateProcessRequest.note(StringUtils.hasLength(errMsg) ? errMsg.describeConstable() : "".describeConstable());
                 } else {
                     updateProcessRequest.status(ProcessStatus.COMPLETED);
                     log.info("call client to update process history with process definition id {} successfully!", processDefinition);
@@ -67,7 +66,7 @@ public class TaskExecutionListener implements ExecutionListener {
             case "delete":
                 break;
             case "take", "throw", "error":
-                String errorDescription = Optional.ofNullable((String) execution.getVariable("errorMessage"))
+                String errorDescription = Optional.ofNullable((String) redisTemplate.opsForHash().get(redisKey, "errorMessage"))
                         .orElse("Unknown error");
                 updateProcessRequest.status(ProcessStatus.FAILED);
                 updateProcessRequest.note(Optional.of(errorDescription));
---
+++ src/main/java/com/qworks/camunda/redis/RedisTemplates.java
@@ -0,0 +1,19 @@
+package com.qworks.camunda.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisTemplates {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        return template;
+    }
+
+}
